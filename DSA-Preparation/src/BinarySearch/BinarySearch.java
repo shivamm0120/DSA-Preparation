5ac4579a7498b7997ecf1121d06fd2583e055f33
shiva <shiva@SHIVAM>
2026-05-21 23:14:30 +0530

    binary search problem

---
+++ DSA-Preparation/src/BinarySearch/BinarySearch.java
@@ -0,0 +1,21 @@
+package BinarySearch;
+
+public class BinarySearch {
+	public int search(int[] nums, int target) {
+        int low=0;
+        int high=nums.length-1;
+
+        while(low<=high){
+            int index=low+(high-low)/2;
+            if(nums[index]==target)return index;
+            else if(nums[index]>target){
+                high=index-1;
+            }
+            else low=index+1;
+        }
+        return -1;
+
+        
+    }
+
+}
